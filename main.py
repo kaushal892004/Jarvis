a323a6289eae057128d2a062c4c9187bbf780626
@Kaushal <155300148+kaushal892004@users.noreply.github.com>
2024-11-20 14:40:39 +0530

    Jarvis Added

---
+++ main.py
@@ -0,0 +1,169 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musiclist
+import requests
+from gtts import gTTS
+import pygame
+import os
+# from openai import OpenAI
+import openai
+import hugchat
+import google.generativeai as genai
+
+#for Taking input from user as a audio/speach
+recognizer = sr.Recognizer()
+
+#used for converting user input to text
+engine = pyttsx3.init() 
+
+#for getting latest news article headline we use API of  " https://newsapi.org/ " for News
+news_api_key = "f3e70ff73d7248c8a9599e991a21a819"
+
+#with pyttsx3 module----------------------------------------------------------
+def speak_old(text):
+    engine.say(text)
+    engine.runAndWait()
+
+#---------------------------------------------------------------------------
+
+#with gTTS module----------------------------------------------------------
+def speak(text):
+
+    tts = gTTS(text)
+    tts.save('temp.mp3') 
+
+    # Initialize Pygame mixer
+    pygame.mixer.init()
+
+    # Load the MP3 file
+    pygame.mixer.music.load('temp.mp3')
+
+    # Play the MP3 file
+    pygame.mixer.music.play()
+
+    # Keep the program running until the music stops playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    
+    pygame.mixer.music.unload()
+    os.remove("temp.mp3")     
+
+#---------------------------------------------------------------------------
+
+#let request handled by GeminiAi ----------------------------------------------
+def aiProcess(command):
+    # try:
+    #     # Configure the API key
+    #     genai.configure(api_key="AIzaSyDOfOQEIGNGtZeuZv4rSRzAuvWtLEvcZnE")
+
+    #     # Initialize the Generative Model
+    #     model = genai.GenerativeModel('gemini-1.5-flash')
+
+    #     # Generate content based on the command
+    #     response = model.generate_content(command)
+
+    #     # Speak out the response text
+    #     speak(response.text)
+
+    # except Exception as e:
+    #     # Handle any other errors that occur
+    try:
+        # Configure the API key
+        genai.configure(api_key="AIzaSyDOfOQEIGNGtZeuZv4rSRzAuvWtLEvcZnE")
+
+        # Initialize the Generative Model
+        model = genai.GenerativeModel('gemini-1.5-flash')
+
+        # Define the system message and user message
+        system_message = "You are a virtual assistant named Jarvis skilled in general tasks like Alexa and Google Cloud. Give short responses please."
+        user_message = command
+
+        # Concatenate the messages into a single string
+        full_prompt = f"System: {system_message}\nUser: {user_message}"
+
+        # Generate content based on the full prompt
+        response = model.generate_content(full_prompt)
+
+        # Speak out the response text
+        speak(response.text)
+
+        return response.text
+
+   
+    except Exception as e:
+        # Handle any other errors that occur
+            print(f"An unexpected error occurred: {e}")
+
+#---------------------------------------------------------------------------
+
+#for process the request of user --------------------------------------------
+def processCommand(c):
+    if  "open google" in c.lower():
+        webbrowser.open("http://google.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("http://youtube.com")  
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://www.linkedin.com/feed/")   
+    elif "open instagram" in c.lower():
+        webbrowser.open("https://www.instagram.com/") 
+    elif "open chatgpt" in c.lower():
+        webbrowser.open("https://chatgpt.com/")
+
+    elif c.lower().startswith("play"): 
+        song = " ".join(c.lower().split(" ")[1:])
+        # song = (c.lower().split(" ")[1:])
+        link = musiclist.music[song]
+        webbrowser.open(link)
+
+    elif "tell me the news" in c.lower():
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&apiKey={news_api_key}")
+        if r.status_code == 200:
+            # Parse the JSON response
+            data = r.json()
+            
+            # Extract the articles
+            articles = data.get('articles', [])
+            
+            # Print the headlines
+            for article in articles:
+                speak(article['title'])
+    else:
+        # output = aiProcess(c)
+        # speak(output)    
+        aiProcess(c)
+
+#---------------------------------------------------------------------------
+
+#main------------------------------------------------------------------------
+if __name__ == "__main__":
+    speak("Initializing Jarvis....")
+    while True:
+        
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        print("Recognizing....")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=3, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            #listining for 'jarvis' word
+            if(word.lower() == "jarvis"):
+                speak("how can i help you")
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+
+
+        except Exception as e:
+            print("No Input! {0}".format(e))
+
+#---------------------------------------------------------------------------            
+    
+
+
---
+++ musiclist.py
@@ -0,0 +1,9 @@
+music = {
+    "sweater": "https://youtu.be/WaYFtIY5oEw?si=YBsTrtQfbsficmTq",
+    "nadan": "https://youtu.be/6MgsHSAcI9k?si=hamN8Q7LeLMfOqL4",
+    "jo bhi mein": "https://youtu.be/d7S_gG48dVM?si=fmtj-Y5D4_CBCwAA",
+    "tamasha playlist": "https://youtu.be/pSn3nqU8wc8?si=TbhJy2lAjDk6EL43",
+    "rockstar playlist": "https://youtu.be/cB-ZRijjIMY?si=rXMjpz6aawMeIDqq",
+    "7 years": "https://youtu.be/TSewEvBob5Q?si=QZpGWG8Gz9BZXmbH",
+    "rabta": "https://youtu.be/zlt38OOqwDc?si=WHE-19v3eyJeXIj-"
+}
